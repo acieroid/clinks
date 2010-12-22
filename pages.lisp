325fc59a724a9d48fdf772f13c04609e4916522a
acieroid <none@none>
2010-12-23 00:00:55 +0100

    printing menu

--- pages.lisp
+++ pages.lisp
@@ -1,6 +1,20 @@
 (in-package :clinks)
 
 (defvar *pages* nil)
+(defvar *menu-elements*
+  '((links "all")
+    (new-link "new")))
+
+(defun print-menu ()
+  (with-html-output-to-string (stream)
+    (:div :id "menu"
+          (flet ((print-link (element)
+                   (let ((link (first element))
+                         (name (second element)))
+                     (htm (:a :href (format nil "/~(~a~)" link) (str name))))))
+            (mapcar (lambda (el) (print-link el) (htm (str " - ")))
+                    (butlast *menu-elements*))
+            (print-link (car (last *menu-elements*)))))))
 
 (defmacro def-blank-page (name &body body)
   (let ((stream (gensym "stream")))
@@ -10,16 +24,17 @@
               (format nil "/~(~a~)" ',name) ',name)
              *dispatch-table*)
        (defun ,name ()
-         (with-output-to-string (,stream)
-           (with-html-output (,stream nil :indent t)
-             ,@body))))))
+         (with-html-output-to-string (,stream nil :indent t)
+           ,@body)))))
 
 (defmacro defpage (name title &body body)
   `(def-blank-page ,name
      (:html
       (:head
-       (:meta :http-equiv "Conte-Type" :content "text/html;charset=utf-8")
+       (:meta :http-equiv "Content-Type" :content "text/html;charset=utf-8")
        (:link :type "text/css" :rel "stylesheet" :href "design.css")
        (:title ,title))
-      (:body ,@body))))
+      (:body
+       (str (print-menu))
+       ,@body))))
 

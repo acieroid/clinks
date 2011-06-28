2f84628d4301334165b4df95ed2959085cb4049c
Quentin Stievenart <quentin.stievenart@gmail.com>
2011-06-28 23:51:10 +0200

    Support users in clinks.js + some refactorisation

--- chrome/clinks.js
+++ chrome/clinks.js
@@ -1,4 +1,50 @@
 var Clinks = {
+    request : function(onresponse, method, page, input, username, password) {
+        req = new XMLHttpRequest();
+        req.onreadystatechange = function() {
+            if (req.readyState == 4) {
+                if (req.status == 0)
+                    onresponse(0, "Error when connecting to the server");
+                else
+                    onresponse(req.status, req.responseText);
+            }
+        }
+        try {
+            req.open(method, page, true);
+            req.setRequestHeader("Content-Type",
+                                 "application/x-www-form-urlencoded");
+            if (username != undefined && password != undefined) {
+                req.setRequestHeader("Authorization", "Basic " +
+                                     btoa(this.username + ":" + this.password));
+            }
+            req.send("input=" + this.representation());
+        }
+        catch (error) {
+            onresponse(0, "Error when connecting to the server");
+        }
+    }
+
+    User : function(username, password) {
+        this.username = username;
+        this.password = password;
+
+        this.response = function(status, str) {
+            alert(str);
+        }
+
+        this.representation = function() {
+            return ("<user>" + 
+                    "<username>" + this.username + "</username>" +
+                    "<password>" + this.password + "</password>" +
+                    "</user>");
+        }
+
+        this.create = function(server) {
+            request(this.onresponse, "POST", server + "/users/" + 
+                    this.username + "/links", this.representation());
+        }
+
+
     Link : function(url, title, tags, notes, username, password) {
         this.url = url;
         this.title = title;
@@ -21,30 +67,9 @@ var Clinks = {
         }
 
         this.create = function(server) {
-            /* the 'this' variable seems to point to the
-             * XMLHttpRequest in the function below */
-            onresponse = this.onresponse;
-            req = new XMLHttpRequest();
-            req.onreadystatechange = function() {
-                if (req.readyState == 4) {
-                    if (req.status == 0)
-                        onresponse(0, "Error when connecting to the server");
-                    else
-                        onresponse(req.status, req.responseText);
-                }
-            }
-            try {
-                req.open("POST", server + "/users/" + this.username + "/links",
-                         true);
-                req.setRequestHeader("Content-Type",
-                                     "application/x-www-form-urlencoded");
-                req.setRequestHeader("Authorization", "Basic " +
-                                     btoa(this.username + ":" + this.password));
-                req.send("input=" + this.representation());
-            }
-            catch (error) {
-                this.onresponse(0, "Error when connecting to the server");
-            }
+            request(this.onresponse, "POST", server + "/users/" +
+                    this.username + "/links", this.representation(),
+                    this.username, this.password);
         }
     }
 }
